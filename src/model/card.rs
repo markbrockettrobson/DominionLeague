0cfdbf67faef554b59ee60b8d4f2b1e911a28ea5
markbrockettrobson <markbrockettrobson@gmail.com>
2022-08-28 16:28:27 +1000

    add card counts

--- src/model/card.rs
+++ src/model/card.rs
@@ -12,6 +12,7 @@ pub struct Card {
     pub name: String,
     pub supply_card: bool,
     pub basic_card: bool,
+    pub card_counts: [u8; 6],  
     pub set_id: u8,
     pub editions: Vec<u8>,
     pub card_tags: Vec<CardTag>,
@@ -56,6 +57,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],  
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -82,6 +84,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],  
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -96,6 +99,7 @@ mod test {
             name: "test name 2".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 2,
             editions: [2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -117,6 +121,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -131,6 +136,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -151,6 +157,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -165,6 +172,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -185,6 +193,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -199,6 +208,7 @@ mod test {
             name: "not test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -219,6 +229,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -233,6 +244,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: false,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -253,6 +265,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: false,
             basic_card: true,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -267,7 +280,44 @@ mod test {
             name: "test name".to_string(),
             supply_card: false,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
+            editions: [1, 2].to_vec(),
+            card_tags: [CardTag::Costs4].to_vec(),
+            kindom_requirements: [[CardTag::AddCardToTopOfDeck].to_vec()].to_vec(),
+            kindom_synergies: [[CardTag::CanReplaceAction].to_vec()].to_vec(),
+            kindom_anti_synergies: [[CardTag::WillReplaceAction].to_vec()].to_vec(),
+            card_types: [CardType::Action, CardType::Attack ,CardType::Curse].to_vec(),
+            art_url: ["www.image1.com".to_string(), "www.image2.com".to_string()].to_vec()
+        };
+        assert!(card_one != card_two);
+    }
+
+    #[test]
+    #[allow(clippy::nonminimal_bool)]
+    fn test_eq_false_card_counts() {
+        let card_one = Card { 
+            id: 1,
+            name: "test name".to_string(),
+            supply_card: true,
+            basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
+            set_id: 1,
+            editions: [1, 2].to_vec(),
+            card_tags: [CardTag::Costs4].to_vec(),
+            kindom_requirements: [[CardTag::AddCardToTopOfDeck].to_vec()].to_vec(),
+            kindom_synergies: [[CardTag::CanReplaceAction].to_vec()].to_vec(),
+            kindom_anti_synergies: [[CardTag::WillReplaceAction].to_vec()].to_vec(),
+            card_types: [CardType::Action, CardType::Attack ,CardType::Curse].to_vec(),
+            art_url: ["www.image1.com".to_string(), "www.image2.com".to_string()].to_vec()
+        };
+        let card_two = Card { 
+            id: 1,
+            name: "test name".to_string(),
+            supply_card: true,
+            basic_card: false,
+            card_counts: [10, 10, 10, 10, 11, 10],
+            set_id: 1,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
             kindom_requirements: [[CardTag::AddCardToTopOfDeck].to_vec()].to_vec(),
@@ -287,6 +337,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -301,6 +352,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 1,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -321,6 +373,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -335,6 +388,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -355,6 +409,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::WillGainBuy].to_vec(),
@@ -369,6 +424,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -389,6 +445,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -403,6 +460,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -423,6 +481,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -437,6 +496,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -457,6 +517,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -471,6 +532,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -491,6 +553,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -505,6 +568,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -525,6 +589,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -539,6 +604,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -559,6 +625,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -572,7 +639,7 @@ mod test {
         
         assert_eq!(
             json,
-            "{\"id\":1,\"name\":\"test name\",\"supply_card\":true,\"basic_card\":false,\"set_id\":0,\"editions\":[1,2],\"card_tags\":[\"Costs4\"],\"kindom_requirements\":[[\"AddCardToTopOfDeck\"]],\"kindom_synergies\":[[\"CanReplaceAction\"]],\"kindom_anti_synergies\":[[\"WillReplaceAction\"]],\"card_types\":[\"Action\",\"Attack\",\"Curse\"],\"art_url\":[\"www.image1.com\",\"www.image2.com\"]}");
+            "{\"id\":1,\"name\":\"test name\",\"supply_card\":true,\"basic_card\":false,\"card_counts\":[10,10,10,10,10,10],\"set_id\":0,\"editions\":[1,2],\"card_tags\":[\"Costs4\"],\"kindom_requirements\":[[\"AddCardToTopOfDeck\"]],\"kindom_synergies\":[[\"CanReplaceAction\"]],\"kindom_anti_synergies\":[[\"WillReplaceAction\"]],\"card_types\":[\"Action\",\"Attack\",\"Curse\"],\"art_url\":[\"www.image1.com\",\"www.image2.com\"]}");
     }
 
     #[test]
@@ -583,6 +650,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -592,7 +660,7 @@ mod test {
             card_types: [CardType::Action, CardType::Attack ,CardType::Curse].to_vec(),
             art_url: ["www.image1.com".to_string(), "www.image2.com".to_string()].to_vec()
         };
-        let json = "{\"id\":1,\"name\":\"test name\",\"supply_card\":true,\"basic_card\":false,\"set_id\":0,\"editions\":[1,2],\"card_tags\":[\"Costs4\"],\"kindom_requirements\":[[\"AddCardToTopOfDeck\"]],\"kindom_synergies\":[[\"CanReplaceAction\"]],\"kindom_anti_synergies\":[[\"WillReplaceAction\"]],\"card_types\":[\"Action\",\"Attack\",\"Curse\"],\"art_url\":[\"www.image1.com\",\"www.image2.com\"]}";
+        let json = "{\"id\":1,\"name\":\"test name\",\"supply_card\":true,\"basic_card\":false,\"card_counts\":[10,10,10,10,10,10],\"set_id\":0,\"editions\":[1,2],\"card_tags\":[\"Costs4\"],\"kindom_requirements\":[[\"AddCardToTopOfDeck\"]],\"kindom_synergies\":[[\"CanReplaceAction\"]],\"kindom_anti_synergies\":[[\"WillReplaceAction\"]],\"card_types\":[\"Action\",\"Attack\",\"Curse\"],\"art_url\":[\"www.image1.com\",\"www.image2.com\"]}";
         let json_set: Card = serde_json::from_str(json).unwrap();
 
         assert_eq!(json_set, card);
@@ -605,6 +673,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -616,7 +685,7 @@ mod test {
         };
         assert_eq!(
             format!("{card:?}"), 
-            "Card { id: 1, name: \"test name\", supply_card: true, basic_card: false, set_id: 0, editions: [1, 2], card_tags: [Costs4], kindom_requirements: [[AddCardToTopOfDeck]], kindom_synergies: [[CanReplaceAction]], kindom_anti_synergies: [[WillReplaceAction]], card_types: [Action, Attack, Curse], art_url: [\"www.image1.com\", \"www.image2.com\"] }");
+            "Card { id: 1, name: \"test name\", supply_card: true, basic_card: false, card_counts: [10, 10, 10, 10, 10, 10], set_id: 0, editions: [1, 2], card_tags: [Costs4], kindom_requirements: [[AddCardToTopOfDeck]], kindom_synergies: [[CanReplaceAction]], kindom_anti_synergies: [[WillReplaceAction]], card_types: [Action, Attack, Curse], art_url: [\"www.image1.com\", \"www.image2.com\"] }");
     }
 
     #[test]
@@ -626,6 +695,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
@@ -654,6 +724,7 @@ mod test {
             name: "test name".to_string(),
             supply_card: true,
             basic_card: false,
+            card_counts: [10, 10, 10, 10, 10, 10],
             set_id: 0,
             editions: [1, 2].to_vec(),
             card_tags: [CardTag::Costs4].to_vec(),
