fa27624701f4b2dc2d0ebffd1f32ca655b49127a
markbrockettrobson <markbrockettrobson@gmail.com>
2022-08-28 23:38:57 +1000

    Change set id to start from 1

--- src/model/data_loaders/card_data_loader.rs
+++ src/model/data_loaders/card_data_loader.rs
@@ -7,7 +7,7 @@ use super::super::card::Card;
 pub static BASE_CARD_JSON_BYTES: &[u8] = include_bytes!("../raw_data/basic_cards.json");
 
 #[allow(dead_code)]
-pub fn get_card_vec() -> Vec<Card> {
+pub fn get_base_card_vec() -> Vec<Card> {
     let set_json = String::from_utf8_lossy(BASE_CARD_JSON_BYTES).into_owned();
     serde_json::from_str(&set_json).unwrap()
 }
@@ -19,13 +19,13 @@ mod test {
 
     #[test]
     fn test_get_cards_vec_loads_at_least_one_set() {
-        let cards = get_card_vec();
+        let cards = get_base_card_vec();
         assert_eq!(cards[0].id, 0);
     }
 
     #[test]
     fn test_no_missing_cards_id() {
-        let cards = get_card_vec();
+        let cards = get_base_card_vec();
         for (index, card) in cards.iter().enumerate() {
             assert_eq!(card.id, index as u8);
         }
