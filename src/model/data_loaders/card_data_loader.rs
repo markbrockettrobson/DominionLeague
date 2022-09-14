66816ed4ae995755e736e5ba81254f03247c38b7
markbrockettrobson <markbrockettrobson@gmail.com>
2022-09-14 20:47:23 +1000

    Add dominion kingdomcards

--- src/model/data_loaders/card_data_loader.rs
+++ src/model/data_loaders/card_data_loader.rs
@@ -5,6 +5,8 @@ use super::super::card::Card;
 
 
 pub static BASE_CARD_JSON_BYTES: &[u8] = include_bytes!("../raw_data/basic_cards.json");
+pub static DOMINION_CARD_JSON_BYTES: &[u8] = include_bytes!("../raw_data/dominion_cards.json");
+
 
 #[allow(dead_code)]
 pub fn get_base_card_vec() -> Vec<Card> {
@@ -12,22 +14,58 @@ pub fn get_base_card_vec() -> Vec<Card> {
     serde_json::from_str(&set_json).unwrap()
 }
 
+#[allow(dead_code)]
+pub fn get_dominion_card_vec() -> Vec<Card> {
+    let set_json = String::from_utf8_lossy(DOMINION_CARD_JSON_BYTES).into_owned();
+    serde_json::from_str(&set_json).unwrap()
+}
+
+#[allow(dead_code)]
+pub fn get_all_card_vec() -> Vec<Card> {
+    let mut all_cards = vec![];
+    all_cards.extend(get_base_card_vec());
+    all_cards.extend(get_dominion_card_vec());
+    all_cards
+}
 
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn test_get_cards_vec_loads_at_least_one_set() {
+    fn test_get_all_card_vec() {
+        let all_cards = get_all_card_vec();
+        let base_cards = get_base_card_vec();
+        let dominion_cards = get_dominion_card_vec();
+
+        assert_eq!(all_cards.len(), base_cards.len() + dominion_cards.len());
+    }
+
+    #[test]
+    fn test_get_base_card_vec_loads_at_least_one_card() {
         let cards = get_base_card_vec();
         assert_eq!(cards[0].id, 0);
     }
 
     #[test]
-    fn test_no_missing_cards_id() {
+    fn test_get_base_card_vec_no_missing_cards_id() {
         let cards = get_base_card_vec();
         for (index, card) in cards.iter().enumerate() {
-            assert_eq!(card.id, index as u8);
+            assert_eq!(card.id, index as u16);
+        }
+    }
+    
+    #[test]
+    fn test_get_dominion_card_vec_loads_at_least_one_card() {
+        let cards = get_dominion_card_vec();
+        assert_eq!(cards[0].id, 100);
+    }
+
+    #[test]
+    fn test_get_dominion_card_vec_no_missing_cards_id() {
+        let cards = get_dominion_card_vec();
+        for (index, card) in cards.iter().enumerate() {
+            assert_eq!(card.id, (index + 100) as u16);
         }
     }
 }
